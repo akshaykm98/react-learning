ad864b992ae502490eefc6768da5f329f135b941
akshay kumar <akshaykumar.m@m.ecsfin.com>
2022-11-09 10:21:57 +0530

    Updates

---
+++ precache-manifest.3d9a98e9c153dee1038eaf924968598a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e79f96056e9001b7dfee",
+    "url": "/react-learning/static/css/main.673c3211.chunk.css"
+  },
+  {
+    "revision": "e79f96056e9001b7dfee",
+    "url": "/react-learning/static/js/main.e79f9605.chunk.js"
+  },
+  {
+    "revision": "6ebea482332ce9046828",
+    "url": "/react-learning/static/js/1.6ebea482.chunk.js"
+  },
+  {
+    "revision": "5ef10902ef3a2ec96a00",
+    "url": "/react-learning/static/js/runtime~main.5ef10902.js"
+  },
+  {
+    "revision": "cdc83922d0abf8795cb44f4c030e381d",
+    "url": "/react-learning/static/media/4.cdc83922.jpg"
+  },
+  {
+    "revision": "6a459caef2d80d1f19040e082475dcd5",
+    "url": "/react-learning/static/media/6.6a459cae.jpg"
+  },
+  {
+    "revision": "ef8550e79ddb7fe0da25dda1366d8cf0",
+    "url": "/react-learning/static/media/259823.ef8550e7.jpg"
+  },
+  {
+    "revision": "b58fcd3778564063cc2d23ede7c51f8c",
+    "url": "/react-learning/static/media/2.b58fcd37.jpg"
+  },
+  {
+    "revision": "8a65132907bf7976fab0132971d7c9d2",
+    "url": "/react-learning/static/media/1.8a651329.jpg"
+  },
+  {
+    "revision": "2fe870cff2451d25c50bb0d4cbdb79bc",
+    "url": "/react-learning/static/media/1.2fe870cf.mp3"
+  },
+  {
+    "revision": "b1a88314edeb0c1c68ce86fbd697e668",
+    "url": "/react-learning/index.html"
+  }
+];
